fd1f87d553a9a9e604b113e099e83ab311ccd08d
arman taghiyan <taghiyan98@gmail.com>
2022-06-08 22:50:20 +0430

    14010319 install laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any?}', function () {
-    return view('main');
-});
+require __DIR__ . '/auth.php';
+
+Route::get('/{any?}', [HomeController::class, 'index'])->middleware('auth')->name('app');
